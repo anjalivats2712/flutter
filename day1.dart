eb90d7002c70de97ed2f29ff8816c9b6b0cdeea7
anjalivats2712 <77090512+anjalivats2712@users.noreply.github.com>
2021-08-19 21:10:23 +0530

    commit

---
+++ day1.dart
@@ -0,0 +1,7 @@
+void main() {
+  for (int i = 0; i < 5; i++) {
+    print('hello ${i + 1}');
+  }
+  print("hello world");
+  print("this is my day 1 learning dart");
+}
---
+++ variable.dart
@@ -0,0 +1,9 @@
+int main() {
+  int a = 89;
+  print(a);
+  bool b = true;
+  print(b);
+  print("maria maria maria maria");
+  print("we dont need to put \ n here it automatically prints in next line");
+  return 0;
+}
